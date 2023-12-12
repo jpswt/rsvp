11b18c00697ae152070d18543d1e42b9cce47d27
jpswt <joel_swt@yahoo.com>
2023-12-12 14:03:34 -0600

    adjusted mobile paragraph width of gift registry component

--- src/components/Gift.jsx
+++ src/components/Gift.jsx
@@ -4,7 +4,7 @@ const Gift = () => {
 	return (
 		<div className="max-w-[1200px] m-auto mb-20">
 			<Banner title="Gift Registry" idName="gift" />
-			<p className="text-center max-w-[50%] m-auto mt-10">
+			<p className="text-center w-[80%] md:max-w-[50%] m-auto mt-10">
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum facere
 				quaerat aliquam minima incidunt eos, eum ab mollitia corporis nobis!
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum facere
