137b6732b754bc010d123d0a48f035d0251cdb8a
jpswt <joel_swt@yahoo.com>
2023-12-12 19:57:22 -0600

    update firebase export

--- src/firebase.js
+++ src/firebase.js
@@ -17,6 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
+export const firestore = getFirestore(app);
 
-export default firestore;
+export default app;
