9aaa8e89b581fce302f1a1de6dc48f9438cbe384
jpswt <joel_swt@yahoo.com>
2024-04-13 07:54:17 -0500

    updated timer

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -2,21 +2,26 @@ import Timer from './Timer';
 
 const Countdown = () => {
 	return (
-		<section className="max-w-[1200px] min-h-[160px] bg-primary m-auto mt-10 flex flex-col lg:flex-row">
+		<section className="m-auto mt-10 flex min-h-[160px] max-w-[1200px] flex-col bg-primary lg:flex-row">
 			<div className="relative">
 				<img
-					className=" w-full lg:w-[600px] h-[160px] object-cover opacity-50"
+					className=" h-[260px] w-full object-cover opacity-50 lg:w-[600px]"
 					src="https://images.unsplash.com/photo-1529636798458-92182e662485?q=80&w=3869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 					alt=""
 				/>
-				<div className="absolute p-4 top-4 w-full flex flex-col items-center lg:w-[400px] gap-4">
-					<h3 className="text-primary font-nav text-2xl ">
-						Countdown Until....
+				<div className="absolute top-8 flex w-full flex-col items-center gap-4 p-4 lg:w-[400px]">
+					<h3 className="font-cursive text-5xl text-primary ">
+						It's Our Wedding Day!
 					</h3>
-					<h2 className="text-primary font-cursive text-5xl">The Big Day</h2>
+					{/* <h2 className="font-cursive text-5xl text-primary">The Big Day</h2> */}
 				</div>
 			</div>
-			<Timer />
+			{/* <div className=" flex w-full items-center justify-center ">
+				<h2 className="  py-10 font-cursive text-5xl text-white lg:text-6xl">
+					It's Our Wedding Day!
+				</h2>
+			</div> */}
+			{/* <Timer /> */}
 		</section>
 	);
 };
