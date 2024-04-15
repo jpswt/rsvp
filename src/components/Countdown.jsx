1b08e450737c2d7d352fe0ea80fb1c0b22ea0e10
jpswt <joel_swt@yahoo.com>
2024-04-15 14:14:47 -0500

    updated timer

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -5,12 +5,12 @@ const Countdown = () => {
 		<section className="m-auto mt-10 flex min-h-[160px] max-w-[1200px] flex-col bg-primary lg:flex-row">
 			<div className="relative">
 				<img
-					className=" h-[260px] w-full object-cover opacity-50 lg:w-[600px]"
+					className=" h-[260px] w-full object-cover opacity-50 lg:w-[1200px]"
 					src="https://images.unsplash.com/photo-1529636798458-92182e662485?q=80&w=3869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 					alt=""
 				/>
-				<div className="absolute top-8 flex w-full flex-col items-center gap-4 p-4 lg:w-[400px]">
-					<h3 className="font-cursive text-5xl text-primary ">
+				<div className="absolute top-20 flex w-full flex-col items-center gap-4 p-4  lg:w-[1200px]">
+					<h3 className="font-cursive text-5xl text-primary md:text-[80px] lg:text-[100px] ">
 						It's Our Wedding Day!
 					</h3>
 					{/* <h2 className="font-cursive text-5xl text-primary">The Big Day</h2> */}
